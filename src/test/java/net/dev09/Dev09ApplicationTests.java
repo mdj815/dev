4abadc88157ae575309df3f9f47da6620f590cce
DESKTOP-F765QL6\Joon <mdj815@naver.com>
2020-09-16 12:43:26 +0900

    initial commit

---
+++ src/test/java/net/dev09/Dev09ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dev09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
