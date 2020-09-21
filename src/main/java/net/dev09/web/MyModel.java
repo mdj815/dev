e246faeec5ec36f0d2ecd62aec9a2f281e7aca3c
DESKTOP-F765QL6\Joon <mdj815@naver.com>
2020-09-21 09:42:28 +0900

    html  기능구현작업

---
+++ src/main/java/net/dev09/web/MyModel.java
@@ -0,0 +1,16 @@
+package net.dev09.web;
+
+public class MyModel {
+	private String name;
+
+	public MyModel(String name) 
+	{
+		super();
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		 return name;
+	}
+}
--- src/main/java/net/dev09/web/WelcomeController.java
+++ src/main/java/net/dev09/web/WelcomeController.java
@@ -1,15 +1,20 @@
 package net.dev09.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model) {
-		System.out.println("name : " + name + "age : " + age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String welcome(Model model) {
+		List<MyModel> repo = Arrays.asList(new MyModel("javajigi"), new MyModel("sanjigi"));
+		
+		
+		model.addAttribute("repo", repo);
+		
 		return "welcome";
 	}
 
